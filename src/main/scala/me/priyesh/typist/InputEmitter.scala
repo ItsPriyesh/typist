bbe382e7e0a40f76eec6b0745b727546d97f08ac
Priyesh Patel <priyeshpatel.96@gmail.com>
2016-09-07 11:40:46 -0400

    Fix race in InputEmitter

--- src/main/scala/me/priyesh/typist/InputEmitter.scala
+++ src/main/scala/me/priyesh/typist/InputEmitter.scala
@@ -1,22 +1,26 @@
 package me.priyesh.typist
 
 import monix.execution.Cancelable
-import monix.reactive.Observable
+import monix.reactive.{Observable, OverflowStrategy}
 import monix.reactive.observers.Subscriber
 import org.scalajs.dom.raw.HTMLInputElement
 import org.scalajs.dom.KeyboardEvent
 
-final class InputEmitter(target: HTMLInputElement, eventType: String = "keyup") extends Observable[String] {
-  override def unsafeSubscribeFn(subscriber: Subscriber[String]): Cancelable = {
-    Observable.unsafeCreate[String] { subscriber =>
-      val listener = (e: KeyboardEvent) => {
-        if (e.key == " ") {
-          subscriber.onNext(target.value.trim)
-          target.value = ""
-        }
-      }
+final class InputEmitter(target: HTMLInputElement) extends Observable[String] {
+  override def unsafeSubscribeFn(subscriber: Subscriber[String]): Cancelable =
+    keyboardEvents(target)
+      .filter(_.key == " ")
+      .map(_ => {
+        val text = target.value.trim
+        target.value = ""
+        text
+      })
+      .unsafeSubscribeFn(subscriber)
+
+  private def keyboardEvents(target: HTMLInputElement, eventType: String = "keyup"): Observable[KeyboardEvent] =
+    Observable.create[KeyboardEvent] (OverflowStrategy.Unbounded) { subscriber =>
+      val listener = (e: KeyboardEvent) => subscriber.onNext(e)
       target.addEventListener(eventType, listener)
       Cancelable(() => target.removeEventListener(eventType, listener))
-    }.unsafeSubscribeFn(subscriber)
-  }
+    }
 }
--- src/main/scala/me/priyesh/typist/WordRenderer.scala
+++ src/main/scala/me/priyesh/typist/WordRenderer.scala
@@ -10,16 +10,16 @@ import org.scalajs.jquery.jQuery
 class WordRenderer(containerSelector: String, strings: Array[String]) extends Observer[Result[String]] {
 
   val container = jQuery(containerSelector)
+  val renderCache: Array[String] = strings.map(toSpan(_, Default))
   var index = 0
-  var renderCache: Array[String] = strings.map(toSpan(_, Default))
   render()
 
   override def onError(ex: Throwable): Unit = ()
   override def onComplete(): Unit = ()
   override def onNext(result: Result[String]): Future[Ack] = {
     renderCache(index) = result match {
-      case Success(e) => println("success " + e); toSpan(e, Correct);
-      case Failure(e, a) => println("failure " + e + " " + a); toSpan(e, Incorrect)
+      case Success(e) => println(s"success: $e"); toSpan(e, Correct)
+      case Failure(e, a) => println(s"failure: ($e, $a)"); toSpan(e, Incorrect)
     }
 
     index += 1
