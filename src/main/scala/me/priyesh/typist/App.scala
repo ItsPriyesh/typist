59744619246ec01953ca2261cd6b6a87d178029b
Priyesh Patel <priyeshpatel.96@gmail.com>
2016-09-07 16:17:21 -0400

    Remove mutable WordRenderer

--- src/main/scala/me/priyesh/typist/App.scala
+++ src/main/scala/me/priyesh/typist/App.scala
@@ -1,5 +1,6 @@
 package me.priyesh.typist
 
+import monix.eval.Coeval
 import org.scalajs.dom
 
 import scala.scalajs.js.JSApp
@@ -33,6 +34,43 @@ object App extends JSApp {
 
     jQuery("#words-container").append(words.mkString(" "))
     StringEvaluator.run(words.toList, new InputEmitter(input))
-      .subscribe(new WordRenderer("#words-container", words))
+      .foldLeftL(Coeval(Words(remaining = words.map(Word(_)).toList))) { (words, result) =>
+        val advanced = words.advance(result match {
+          case Success(s) => Correct
+          case Failure(e, a) => Incorrect
+        })
+        jQuery("#words-container").empty()
+        jQuery("#words-container").append(words.render)
+        advanced
+      }.runAsync
+    // .subscribe(new WordRenderer("#words-container", words))
   }
+
+  case class Words(completed: List[Word] = Nil, remaining: List[Word]) {
+    def advance(newState: State): Words =
+      if (remaining isEmpty) this else Words(completed :+ remaining.head.copy(state = newState), remaining.tail)
+
+    def render: String = (completed ++ remaining).map(_.render).mkString(" ")
+  }
+
+  case class Word(string: String, state: State = Default) {
+    lazy val render: String = s"<span style='color: ${state.color}'>$string</span>"
+  }
+
+  sealed trait State {
+    def color: String
+  }
+
+  case object Default extends State {
+    def color: String = "#abb2bf"
+  }
+
+  case object Correct extends State {
+    def color: String = "#98c379"
+  }
+
+  case object Incorrect extends State {
+    def color: String = "#e06c75"
+  }
+
 }
--- src/main/scala/me/priyesh/typist/WordRenderer.scala
+++ src/main/scala/me/priyesh/typist/WordRenderer.scala
@@ -10,41 +10,28 @@ import org.scalajs.jquery.jQuery
 class WordRenderer(containerSelector: String, strings: Array[String]) extends Observer[Result[String]] {
 
   val container = jQuery(containerSelector)
-  val renderCache: Array[String] = strings.map(toSpan(_, Default))
+  //val renderCache: Array[String] = strings.map(toSpan(_, Default))
   var index = 0
   render()
 
   override def onError(ex: Throwable): Unit = ()
   override def onComplete(): Unit = ()
   override def onNext(result: Result[String]): Future[Ack] = {
-    renderCache(index) = result match {
-      case Success(e) => println(s"success: $e"); toSpan(e, Correct)
-      case Failure(e, a) => println(s"failure: ($e, $a)"); toSpan(e, Incorrect)
-    }
+   //a renderCache(index) = result match {
+      //case Success(e) => println(s"success: $e"); toSpan(e, Correct)
+      //case Failure(e, a) => println(s"failure: ($e, $a)"); toSpan(e, Incorrect)
+  //  }
 
     index += 1
     render()
     Continue
   }
 
-  def toSpan(str: String, state: State): String = s"<span style='color: ${state.color}'>$str</span>"
 
   def render() = {
     container.empty()
-    container.append(renderCache.mkString(" "))
+   // container.append(renderCache.mkString(" "))
   }
 
-  sealed trait State { def color: String }
 
-  case object Default extends State {
-    def color: String = "#abb2bf"
-  }
-
-  case object Correct extends State {
-    def color: String = "#98c379"
-  }
-
-  case object Incorrect extends State {
-    def color: String = "#e06c75"
-  }
 }
