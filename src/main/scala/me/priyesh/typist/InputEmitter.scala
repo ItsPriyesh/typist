d5345f40f44ad63fe38ee8d7d401cc7f8b6fe38b
Priyesh Patel <priyeshpatel.96@gmail.com>
2016-09-29 17:15:32 -0400

    Listen for keydown instead of keyup

--- src/main/scala/me/priyesh/typist/InputEmitter.scala
+++ src/main/scala/me/priyesh/typist/InputEmitter.scala
@@ -7,19 +7,22 @@ import org.scalajs.dom.raw.HTMLInputElement
 import org.scalajs.dom.KeyboardEvent
 
 final case class InputEmitter(target: HTMLInputElement, onStart: () => Unit) extends Observable[String] {
+
   override def unsafeSubscribeFn(subscriber: Subscriber[String]): Cancelable =
     keyboardEvents(target)
       .doOnStart(_ => onStart())
       .filter(_.key == " ")
       .map(_ => {
-        // TODO: Fix race (next item emits before target is cleared)
+        // TODO: Fix race:
+        // Space followed immediately by next char will include the next char in previous word
+        // ie: Target is not cleared before next word
         val text = target.value.trim
         target.value = ""
         text
       })
       .unsafeSubscribeFn(subscriber)
 
-  private def keyboardEvents(target: HTMLInputElement, eventType: String = "keyup"): Observable[KeyboardEvent] =
+  private def keyboardEvents(target: HTMLInputElement, eventType: String = "keydown"): Observable[KeyboardEvent] =
     Observable.create[KeyboardEvent] (OverflowStrategy.Unbounded) { subscriber =>
       val listener = (e: KeyboardEvent) => subscriber.onNext(e)
       target.addEventListener(eventType, listener)
